216b58b8e2a786c8a7e9ef2da17c68abb9549706
KOTIPALLI PRAVEEN KUMAR <2200032132@kluniversity.in>
2026-06-27 18:42:03 +0530

    version 0.2

---
+++ ApexIQ/force-app/main/default/lwc/apexIQAttention/apexIQAttention.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class ApexIQAttention extends LightningElement {}
---
+++ ApexIQ/force-app/main/default/lwc/apexIQHeader/apexIQHeader.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class ApexIQHeader extends LightningElement {}
