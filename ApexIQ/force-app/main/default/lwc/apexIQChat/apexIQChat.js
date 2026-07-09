0969442f91a1a6902b89e85b3c660cb5dc8ebe63
KOTIPALLI PRAVEEN KUMAR <2200032132@kluniversity.in>
2026-07-09 09:57:44 +0530

    added apexIQChar LWC

--- ApexIQ/force-app/main/default/lwc/apexIQChat/apexIQChat.js
+++ ApexIQ/force-app/main/default/lwc/apexIQChat/apexIQChat.js
@@ -1,3 +1,34 @@
 import { LightningElement } from 'lwc';
+import askAI from '@salesforce/apex/GeminiController.askAI';
 
-export default class ApexIQChat extends LightningElement {}
+export default class ApexIQChat extends LightningElement {
+
+    prompt = '';
+    response = '';
+
+    handleChange(event) {
+        this.prompt = event.target.value;
+    }
+
+    async askGemini() {
+
+        try {
+
+            this.response = 'Thinking...';
+
+            const result = await askAI({
+                prompt: this.prompt
+            });
+
+            this.response = result;
+
+        } catch (error) {
+
+            this.response =
+                JSON.stringify(error);
+
+        }
+
+    }
+
+}
